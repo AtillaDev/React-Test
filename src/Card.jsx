ad5f20a71be7e46713c936abd713f9e2e6419172
Atila <atiladev@abv.bg>
2025-02-04 21:38:54 +0200

    experimenting

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Atila</h2>
+            <p>Studying in Technical University of Sofia</p>
+        </div>
+    )
+
+}
+
+export default Card
