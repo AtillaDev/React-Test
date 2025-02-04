45c6571a2d8922c6c02ec8da51c482c8b2c442e5
Atila <atiladev@abv.bg>
2025-02-04 22:25:22 +0200

    experimenting

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
+
 function Card() {
     return(
         <div className="card">
-            <img src="" alt="" />
-            <h2>Atila</h2>
-            <p>Studying in Technical University of Sofia</p>
+            <img className="card-image" src="https://avatars.githubusercontent.com/u/87375212?v=4" height={"200px"} alt="profile picture" />
+            <h2 className="card-title">Atila</h2>
+            <p className="card-text">Studying in Technical University of Sofia</p>
         </div>
     )
 
